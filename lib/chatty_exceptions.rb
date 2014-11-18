6644a8efe3134cdfe8e8aa8a833c609c64ae8a3a
Kerri Miller <kerrizor@kerrizor.com>
2014-11-18 13:19:36 -0800

    send message string of exception to Mac::TTS

--- lib/chatty_exceptions.rb
+++ lib/chatty_exceptions.rb
@@ -1,5 +1,17 @@
 require "chatty_exceptions/version"
 
-module ChattyExceptions
-  # Your code goes here...
+require 'mactts'
+
+class Exception
+  attr_reader :initial_message
+
+  def initialize(message)
+    @initial_message = message
+    super
+  end
+
+  def to_s
+    Mac::TTS.say(@initial_message, :victoria)
+    @initial_message
+  end
 end
